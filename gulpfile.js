875d088954a6731f7d25d6d445fc821a80b499ab
HC <chunhuang@zdongpin.com>
2016-01-13 10:38:06 +0800

    module added

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,19 @@
 /*create by HC*/
 var gulp = require('gulp');
+// 管理gulp错误进程
+var plumber = require('gulp-plumber');
+// 创建browser-sync实例
 var bs = require('browser-sync').create();
+// 引入less，可创建sourcemap
+var less = require('gulp-less-sourcemap');
+// 添加浏览器前缀
+var cssprefix = require('gulp-autoprefixer');
+// 编译jade
+var jade = require('jade');
+// js语法检查
+var jshint = require('gulp-jshint');
+// 压缩js
+var ugjs = required('gulp-uglifyjs');
+// 压缩css
+var ugcss = required('gulp-uglifycss');
 
