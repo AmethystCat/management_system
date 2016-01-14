1997f91a83e127e94f683fbd69432e71cbbed6c7
unknown <1772655815@qq.com>
2016-01-14 13:45:28 +0800

    index page developed

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,18 @@
 /**
  * Created by john on 2016/1/13.
  */
-console.log('hello hcc');
+$(function () {
+    console.log('qweqweqwe');
+    /* nav click
+    * ------------*/
+    $('ul.nav-list').on('click','li',function(){
+        if ( $(this).hasClass('submenu') &&  ) {
+            $(this).children('ul').show();
+        } else {
+            $(this).siblings('li').find('ul').hide();
+        }
+        $(this).addClass('active').parents('li').removeClass('active');
+        //console.log('qweqweqwe');
+        $(this).siblings('li').removeClass('active');
+    })
+});
