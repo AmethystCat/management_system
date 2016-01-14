ffaa6f89302f8c53f8b0b0d01e0bb0324a6e5159
HC <chunhuang@zdongpin.com>
2016-01-14 18:28:04 +0800

    table completed and optimizate index page

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,58 @@
 /**
  * Created by john on 2016/1/13.
  */
+ (function (j) {
+    j.fn.extend({
+        accordion: function () {
+            return this.each(function () {
+                function b(c, b) {
+                    $(c).parent(d).siblings().removeClass(e).children(f).slideUp(g);
+                    //$(c).parent(d).siblings().removeClass(e).find('.fa-chevron-right').addClass('fa-chevron-down');
+                   // $(c).parent(d).find('.fa-chevron-right').addClass('fa-chevron-down');
+                    $(c).siblings(f)[b || h](b == "show" ? g : !1, function () {
+                        $(c).siblings(f).is(":visible") ? $(c).parents(d).not(a.parents()).addClass(e) : $(c).parent(d).removeClass(e);
+                        b == "show" && $(c).parents(d).not(a.parents()).addClass(e);
+                        $(c).parents().show()
+                    })
+                }
+                var a = $(this),
+                    e = "current",
+                    h = "slideToggle",
+                    f = "ul, div",
+                    g = "slow",
+                    d = "li";
+                if (a.data("accordiated")) return !1;
+                $.each(a.find("ul, li>div"),function () {
+                        $(this).data("accordiated", !0);
+                        $(this).hide()
+                    });
+                $.each(a.find("a"), function () {
+                    $(this).click(function () {
+                        //$(this).find(' .fa-chevron-right').removeClass('fa-chevron-down');
+                        b(this, h)
+                    });
+                    $(this).bind("activate-node", function () {
+                        a.find(f).not($(this).parents()).not($(this).siblings()).slideUp(g);
+                        b(this, "slideDown");
+
+                    })
+                });
+                var i = location.hash ? a.find("a[href=" + location.hash + "]")[0] : a.find("li.current a")[0];
+                i && b(i, !1)
+            })
+        }
+    })
+})(jQuery);
+
 $(function () {
     console.log('qweqweqwe');
     /* nav click
     * ------------*/
-    $('ul.nav-list').on('click','li',function(){
-        if ( $(this).hasClass('submenu') &&  ) {
-            $(this).children('ul').show();
-        } else {
-            $(this).siblings('li').find('ul').hide();
-        }
-        $(this).addClass('active').parents('li').removeClass('active');
-        //console.log('qweqweqwe');
-        $(this).siblings('li').removeClass('active');
-    })
+    $('ul.nav-list').accordion();
+    $('ul.nav-list li a').on('click', function (e) {
+        e.preventDefault();
+        $('ul.nav-list li ').removeClass('active');
+        $(this).parent().addClass('active');
+        $(this).parents('.submenu').addClass('current').find('ul').css('display','block')
+    });
 });
