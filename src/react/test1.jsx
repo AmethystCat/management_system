5aaa10cb777f7764ef7635eeca32694eb50846c2
HC <chunhuang@zdongpin.com>
2016-01-29 18:20:15 +0800

    add H.js and server.js

---
+++ src/react/test1.jsx
@@ -0,0 +1,19 @@
+/**
+ * Created by john on 2016/1/26.
+ */
+
+import React from "react";
+
+let Hello = React.createClass({
+
+    render(){
+        return (
+            <div>
+                <h1>Hello react,coasdasdsme on</h1>
+                <input type="text"/>
+            </div>
+        );
+    }
+});
+
+module.exports = Hello;
