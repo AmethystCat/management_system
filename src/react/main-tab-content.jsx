421086fab1b8532b22a2de1cc01093a0f1be308e
HC <chunhuang@zdongpin.com>
2016-02-20 11:44:04 +0800

    add money and messcontrol entry

--- src/react/main-tab-content.jsx
+++ src/react/main-tab-content.jsx
@@ -2,6 +2,8 @@ import React from  "react";
 import Home from  "./pages/home/main.jsx";
 import Deposit from  "./pages/deposit/main.jsx";
 import WithDraw from  "./pages/withdraw/main.jsx";
+import MoneyCheck from  "./pages/moneycheck/main.jsx";
+import MessControl from  "./pages/personal_mess_manage/main.jsx";
 
 let TabContentControl = React.createClass({
     render(){
@@ -13,6 +15,10 @@ let TabContentControl = React.createClass({
             panelContent = <Deposit/>
         } else if (url.indexOf('withdraw') != -1 ) {
             panelContent = <WithDraw/>;
+        } else if (url.indexOf('money') != -1 ) {
+            panelContent = <MoneyCheck/>;
+        } else if (url.indexOf('messageManagement') != -1 ) {
+            panelContent = <MessControl/>;
         }
         return (
             <div>
